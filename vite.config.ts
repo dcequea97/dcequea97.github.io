543c496033d2dbacf6b3e0729d0ba764859cd599
David Cequea <dcequea@technotypes.com>
2025-12-02 18:42:03 -0400

    test vite github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(() => {
     // const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base: '/username.github.io/',
+      base: '/',
       // server: {
       //   port: 443,
       //   host: '0.0.0.0',
